358a0fcfbd13eabc238f7ff603b932ba5c67f7fb
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-06-28 16:45:28 +0530

    Create Assignment Operators.cpp

---
+++ Assignment Operators.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    // 2 is assigned to a
+    a = 2;
+
+    // 7 is assigned to b
+    b = 7;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "\nAfter a += b;" << endl;
+
+    // assigning the sum of a and b to a
+    a += b;  // a = a +b
+    cout << "a = " << a << endl;
+
+    return 0;
+}
