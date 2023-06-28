fb135d2046f979b9c12015960d65579d4cb8311b
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-06-28 16:32:16 +0530

    Create declaration variables .cpp

---
+++ declaration variables .cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x=100;
+    x=200;
+    x=3000;
+    x=1799;
+    x=4000;
+    cout <<"Your output X:"<<x<<endl;
+    return 0;
+}
+//Guess what is the output it is very easy but confusing problem;
