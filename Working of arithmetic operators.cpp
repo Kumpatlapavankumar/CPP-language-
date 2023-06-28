87bd0672a09040cc62b7071bc86872982e09dc0f
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-06-28 16:43:15 +0530

    Update Working of arithmetic operators.cpp

--- Working of arithmetic operators.cpp
+++ Working of arithmetic operators.cpp
@@ -1,21 +1,27 @@
+
 #include <iostream>
 using namespace std;
-int main()
-{
+
+int main() {
     int a,b,c;
     cout<<"Enter a value:";
     cin>>a;
     cout<<"\nEnter b value:";
     cin>>b;
-    c = a+b;
-    printf("a+b = %d \n",c);
-    c = a-b;
-    printf("a-b = %d \n",c);
-    c = a*b;
-    printf("a*b = %d \n",c);
-    c = a/b;
-    printf("a/b = %d \n",c);
-    c = a%b;
-    printf("Remainder when a divided by b = %d \n",c);
+    // printing the sum of a and b
+    cout << "a + b = " << (a + b) << endl;
+
+    // printing the difference of a and b
+    cout << "a - b = " << (a - b) << endl;
+
+    // printing the product of a and b
+    cout << "a * b = " << (a * b) << endl;
+
+    // printing the division of a by b
+    cout << "a / b = " << (a / b) << endl;
+
+    // printing the modulo of a by b
+    cout << "a % b = " << (a % b) << endl;
+
     return 0;
 }
