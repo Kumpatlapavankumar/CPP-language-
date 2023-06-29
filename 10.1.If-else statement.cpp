65f729c4797a7642f26ee4d4c2e561bf7a6e6d17
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-06-29 22:29:07 +0530

    Create 10.1.If-else statement.cpp

---
+++ 10.1.If-else statement.cpp
@@ -0,0 +1,15 @@
+// C++ program to illustrate if-else statement
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i = 20;
+
+	if (i < 15)
+		cout << "i is smaller than 15";
+	else
+		cout << "i is greater than 15";
+
+	return 0;
+}
