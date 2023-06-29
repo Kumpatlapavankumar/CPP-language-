cde1d7b4c291ed5b3a1b448a2617ed53401bc2f1
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-06-29 22:26:38 +0530

    Update 9.1.If statement.cpp

--- 9.1.If statement.cpp
+++ 9.1.If statement.cpp
@@ -1,1 +1,13 @@
+// C program to illustrate If statement
+#include <stdio.h>
+
+int main()
+{
+	int i = 10;
+
+	if (i > 15) {
+		printf("10 is greater than 15");
+	}
+	printf("I am Not in if");
+}
 
