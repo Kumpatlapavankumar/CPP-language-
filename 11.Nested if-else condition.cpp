6c14def0559b45f316add9933753225a080b9ba7
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-06-29 22:30:05 +0530

    Create 11.Nested if-else condition.cpp

---
+++ 11.Nested if-else condition.cpp
@@ -0,0 +1,11 @@
+if (condition1) 
+{
+   // Executes when condition1 is true
+   if (condition2) 
+   {
+      // Executes when condition2 is true
+   }
+   else
+   {
+         // Executes when condition2 is false
+}
