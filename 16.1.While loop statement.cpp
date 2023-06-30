3dc6eb481ba79df5738d793a675e8f9205fb9999
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-06-30 09:54:55 +0530

    Create 16.1.While loop statement.cpp

---
+++ 16.1.While loop statement.cpp
@@ -0,0 +1,19 @@
+// C++ program to Demonstrate while loop
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// initialization expression
+	int i = 1;
+
+	// test expression
+	while (i < 6) {
+		cout << "Hello World\n";
+
+		// update expression
+		i++;
+	}
+
+	return 0;
+}
