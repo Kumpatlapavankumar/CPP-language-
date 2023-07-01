ac80df222543cf7680845ce3ea67de1a895f2fa3
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-07-01 21:22:46 +0530

    Create 17.1.Do-while loop statement.cpp

---
+++ 17.1.Do-while loop statement.cpp
@@ -0,0 +1,19 @@
+// C++ program to Demonstrate do-while loop
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i = 2; // Initialization expression
+
+	do {
+		// loop body
+		cout << "Hello World\n";
+
+		// update expression
+		i++;
+
+	} while (i < 1); // test expression
+
+	return 0;
+}
