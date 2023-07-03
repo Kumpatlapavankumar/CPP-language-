5ad54fc938944d0b1415b6db89f60d1a0adfb097
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-07-03 18:23:14 +0530

    Update 20.2.Function default parameter.cpp

--- 20.2.Function default parameter.cpp
+++ 20.2.Function default parameter.cpp
@@ -10,3 +10,31 @@ int main(){
 int pavan(int l,int b,int h){
     return l*b*h;
 }
+
+//
+
+// CPP Program to demonstrate Default Arguments
+#include <iostream>
+using namespace std;
+ 
+// A function with default arguments,
+// it can be called with
+// 2 arguments or 3 arguments or 4 arguments.
+int sum(int x, int y, int z = 0, int w = 0) //assigning default values to z,w as 0
+{
+    return (x + y + z + w);
+}
+ 
+// Driver Code
+int main()
+{
+    // Statement 1
+    cout << sum(10, 15) << endl;
+   
+    // Statement 2
+    cout << sum(10, 15, 25) << endl;
+   
+    // Statement 3
+    cout << sum(10, 15, 25, 30) << endl;
+    return 0;
+}
