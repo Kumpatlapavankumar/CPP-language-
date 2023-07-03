47a22861ade3b7d069809504d47137fd67673a29
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-07-03 17:41:02 +0530

    Create 20.2.local variables in functions.cpp

---
+++ 20.2.Function in local variables.cpp
@@ -0,0 +1,13 @@
+//Local variables-We donot use in another function;
+#include<iostream>
+using namespace std;
+int pavan();
+int main(){
+    int x=100;
+    cout<<"x:"<<x;
+    pavan();
+    return 0;
+}
+int pavan(){
+    cout<<"x:"<<x;
+}
