a074ccf92d0f26f4b742fb2382301552373115f5
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-07-03 16:51:25 +0530

    Create 20.1.Function parameters.cpp

---
+++ 20.1.Function parameters.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int pavan(int l,int b,int h);
+int main(){
+    cout<<pavan(20,30,40)<<endl;
+    return 0;
+}
+int pavan(int l,int b,int h){
+    return l*b*h;
+}
