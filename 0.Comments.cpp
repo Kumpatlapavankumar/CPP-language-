e1c74060983b2801aab5cb1ac9e081188c136c88
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-07-04 11:58:28 +0530

    Create 0.Comments.cpp

---
+++ 0.Comments.cpp
@@ -0,0 +1,23 @@
+//comment
+//Comments means-It is not print but it is in that code
+//Comments-two type
+//single line-//
+#include<iostream>
+using namespace std;
+int main(){
+    //cout<<"HEllO GUYS";
+    return 0;
+}
+//multi line-/*             
+
+
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    /*cout<<"HEllO GUYS";
+    cout<<"HOW ARE YOU GUYS";
+    cout<<"THANK YOU ALL"*/
+    return 0;
+}
