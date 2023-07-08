fee85701660ee1d78552729615fd64e2ede748b1
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-07-08 10:55:24 +0530

    Create 23.2.classes and objects by using public.cpp

---
+++ 23.2.classes and objects by using public.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+class apple{
+  public:
+    string name;
+};
+int main(){
+    apple appleobj;
+    appleobj.name="pavan";
+    cout<<"My name is : "<<appleobj.name;
+    return 0;
+}
