5e7784a4d939df1036f4640fdb3800c1e340b6e3
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-07-10 16:53:10 +0530

    Create 25.Declaration of constant variables.cpp

---
+++ 25.Declaration of constant variables.cpp
@@ -0,0 +1,15 @@
+include<iostream>
+using namespace std;
+
+int main()
+{
+    //normal variable(It can be changed anytime)
+    int x=15;
+    cout <<"before change X:"<<x<<endl;;
+    x=2848;
+    cout <<"after change X:"<<x<<endl;
+    //constant variable (It can't be changed )
+    const int y=47;
+    cout<<"constant variable y="<<y<<endl;
+    return 0;
+}
