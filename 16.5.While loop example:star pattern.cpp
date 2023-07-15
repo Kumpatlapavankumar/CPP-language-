d6916a14bdc21d45cbd33ccd92dca31868e616dd
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-07-15 13:46:32 +0530

    Create 16.5.While loop example:star pattern.cpp

---
+++ 16.5.While loop example:star pattern.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter n value:";
+	cin>>n;
+	int i=1;
+	while(i<=n){
+		int j=1;
+		while(j<=n){
+			cout<<"*";
+			j++;
+		}
+		cout<<"\n";
+		i++;
+	}
+	return 0;
+}
+//second pattern;
+
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter n value:";
+	cin>>n;
+	int i=1;
+	while(i<=n){
+		int j=1;
+		while(j<=n){
+			cout<i;
+			j++;
+		}
+		cout<<"\n";
+		i++;
+	}
+	return 0;
+}
+/*
+if you enter 3
+then output like 
+111
+222
+333
+*/
+
