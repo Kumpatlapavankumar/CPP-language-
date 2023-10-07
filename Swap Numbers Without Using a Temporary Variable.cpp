c3605b90007582af830c2acd804be52e634e7220
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-10-07 18:34:42 +0530

    Update and rename Jdjxk.cpp to Swap Numbers Without Using a Temporary Variable.cpp

---
+++ Swap Numbers Without Using a Temporary Variable.cpp
@@ -0,0 +1,23 @@
+// C++ program to swap two
+// numbers without using 3rd
+// variable
+#include <bits/stdc++.h>
+using namespace std;
+
+// Driver code
+int main()
+{
+	int a = 2, b = 3;
+
+	cout << "Before swapping a = " << a << " , b = " << b
+		<< endl;
+
+	// applying algorithm
+	b = a + b;
+	a = b - a;
+	b = b - a;
+
+	cout << "After swapping a = " << a << " , b = " << b
+		<< endl;
+	return 0;
+}
